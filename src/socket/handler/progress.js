b3e57c0099a75418ded27aa2352e4941c544135a
schizolab <schizolab@yeah.net>
2023-12-11 00:58:15 +0800

    Thinking about what progress should be

--- src/socket/handler/progress.js
+++ src/socket/handler/progress.js
@@ -4,6 +4,12 @@ import clientIdentities from '../identity.js'
 const logger = log4js.getLogger('socket')
 
 export default function handleProgress(socket) {
+    // example of progress
+    // {
+    //     taskId:'123123123',
+    //     action:'fetching from URL',
+    //     progressPercentage:'95.3'
+    // }
     socket.on('set progress', (progress) => {
         let name = clientIdentities.get(socket.id)
         if (!name) {
