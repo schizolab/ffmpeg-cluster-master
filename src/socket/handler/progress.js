7ca8e9470b24094b68bc7503bfc974277bc86190
schizolab <schizolab@yeah.net>
2023-12-12 00:02:22 +0800

    Test set progress success

--- src/socket/handler/progress.js
+++ src/socket/handler/progress.js
@@ -4,14 +4,22 @@ const logger = log4js.getLogger('socket')
 import clientIdentities from '../identity.js'
 
 export default function handleProgress(socket) {
-    socket.on('set progress', ({ taskId, action, progressPercentage, callback }) => {
+    socket.on('set progress', ({ taskId, action, progressPercentage }, callback) => {
         let name = clientIdentities.get(socket.id)
         if (!name) {
             logger.error(`set progress: socket ${socket.id} did not have a name`)
+            callback({
+                success: false,
+                message: 'socket did not have a name'
+            })
             return
         }
 
-        logger.trace(`set progress: ${name} is doing ${action} at ${JSON.stringify(progress)}`)
+        callback({
+            success: true
+        })
+
+        logger.trace(`set progress: ${name} is doing ${action} for ${taskId} at ${progressPercentage}%`)
 
         // pushing progress to UI maybe
 
