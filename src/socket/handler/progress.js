a6e2674802839a8c54ede1b17bb53c8f12783a92
schizolab <schizolab@yeah.net>
2023-12-11 16:43:46 +0800

    Make the code self-explanatory

--- src/socket/handler/progress.js
+++ src/socket/handler/progress.js
@@ -4,20 +4,14 @@ import clientIdentities from '../identity.js'
 const logger = log4js.getLogger('socket')
 
 export default function handleProgress(socket) {
-    // example of progress
-    // {
-    //     taskId:'123123123',
-    //     action:'fetching from URL',
-    //     progressPercentage:'95.3'
-    // }
-    socket.on('set progress', (progress) => {
+    socket.on('set progress', ({ taskId, action, progressPercentage }) => {
         let name = clientIdentities.get(socket.id)
         if (!name) {
             logger.error(`set progress: socket ${socket.id} did not have a name`)
             return
         }
 
-        logger.trace(`set progress: ${name} reported progress of ${JSON.stringify(progress)}`)
+        logger.trace(`set progress: ${name} is doing ${action} at ${JSON.stringify(progress)}`)
 
         // pushing progress to UI maybe
 
