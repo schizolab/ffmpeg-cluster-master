8511c3765d1bed2007480351f730cd88c743dc33
schizolab <schizolab@yeah.net>
2023-12-14 03:31:13 +0800

    Forgot to change SQL path after restructuring folders

--- src/db/sqlite.js
+++ src/db/sqlite.js
@@ -1,7 +1,7 @@
 import Database from 'better-sqlite3';
 
 import fs from 'fs';
-const schema = fs.readFileSync('./src/db/tasking/temp_db.sql', 'utf8');
+const schema = fs.readFileSync('./src/db/temp_db.sql', 'utf8');
 
 // if db were to be deleted, the video status is rebuilt from s3
 export const db = new Database('./temp/db.sqlite');
