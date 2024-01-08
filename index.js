2f90d703af65d01fdbcc5a308e4b0b22003a00cf
schizolab <schizolab@yeah.net>
2024-01-08 20:55:33 +0800

    Move database loading into its own file

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
+import log4js from "./src/logging.js";
+const logger = log4js.getLogger()
+
 import 'dotenv/config'
 import { Command } from "commander";
 const program = new Command()
-import { readFileSync } from 'fs';
 
 import http from 'http'
 import { createExpressApp } from "./src/rest/expressApp.js";
 import { attachSocket } from "./src/socket/socket.js";
 
-import log4js from "./src/logging.js";
-
-import { iterateOverSourceVideos, iterateOverDestinationVideos } from "./src/s3/iteration.js";
-import { getVideo, getVideoByPrefix, insertVideo, updateVideoByFileKey } from './src/db/videos.js';
 
 import { taskWatchdog } from './src/tasking/task.js';
 
-import { extractFileName, extractCleanFileName } from './src/s3/utils.js';
+import { setIncludeFilePath, loadDatabaseAsync } from './src/db/init.js'
 
 program
     .name('ffmpeg cluster master')
@@ -26,56 +24,11 @@ program
     .description('Start the server')
     .option('-p, --port [port]', 'path to bind, default 50001')
     .option('-i, --include-file [includeFile]', 'specify a file that contains a list of file names to process')
-    .action(async ({ port = 50001, includeFile: includeFilePath }) => {
-        const logger = log4js.getLogger()
-
-        // init the database
-        {
-            // load include file into a list of file names
-            let includeFiles = []
-            if (includeFilePath) {
-                try {
-                    includeFiles = readFileSync(includeFilePath)
-                        .toString('utf-8')
-                        .split('\n')
-                        .map((line) => line.trim())
-                } catch (error) {
-                    logger.error(`failed to load include file ${includeFilePath}`)
-                }
-            }
-
-            // iterate over source videos
-            logger.info('checking videos in source bucket, inserting if not exists')
-            await iterateOverSourceVideos((key) => {
-                // include file check
-                if (includeFiles.length > 0) {
-                    const cleanFileName = extractCleanFileName(key)
-                    if (!includeFiles.includes(cleanFileName)) { // if not in the include file, skip
-                        return
-                    }
-                }
-
-                const fileName = extractFileName(key)
-
-                // check if the video is already in the database
-                const video = getVideo({ file_key: fileName })
-                if (!video) { // if not, insert it
-                    insertVideo({ file_key: fileName, status: 'unprocessed' })
-                }
-            })
-
-            // iterate over destination videos
-            logger.info('checking videos in destination bucket, marking as done ifs exists')
-            await iterateOverDestinationVideos((key) => {
-                const fileName = extractCleanFileName(key)
-
-                // check if the video is already in the database
-                const video = getVideoByPrefix(fileName)
-                if (video && video.status !== 'completed') { // if exists, mark it as done
-                    updateVideoByFileKey({ file_key: video.file_key, status: 'completed' })
-                }
-            })
-        }
+    .action(async ({ port = 50001, includeFile }) => {
+        // populate the SQLite database using indexing data from S3
+        // knowing which are processed and which are not yet processed
+        setIncludeFilePath(includeFile)
+        await loadDatabaseAsync()
 
         // start the task watchdog
         setInterval(async () => {
---
+++ src/db/init.js
@@ -0,0 +1,63 @@
+import log4js from "../logging.js";
+const logger = log4js.getLogger()
+
+import { iterateOverSourceVideos, iterateOverDestinationVideos } from "../s3/iteration.js";
+import { getVideo, getVideoByPrefix, insertVideo, updateVideoByFileKey } from '../db/videos.js';
+import { extractFileName, extractCleanFileName } from '../s3/utils.js';
+import { readFileSync } from 'fs';
+
+let includeFilePath = ''
+
+// check on s3 endpoint for videos to process and videos done
+// subtract videos not in the includeFiles
+// insert new videos into the database
+export async function loadDatabaseAsync() {
+    // load include file into a list of file names
+    let includeFiles = []
+    if (includeFilePath) {
+        try {
+            includeFiles = readFileSync(includeFilePath)
+                .toString('utf-8')
+                .split('\n')
+                .map((line) => line.trim())
+        } catch (error) {
+            logger.error(`failed to load include file ${includeFilePath}`)
+        }
+    }
+
+    // iterate over source videos
+    logger.info('checking videos in source bucket, inserting if not exists')
+    await iterateOverSourceVideos((key) => {
+        // include file check
+        if (includeFiles.length > 0) {
+            const cleanFileName = extractCleanFileName(key)
+            if (!includeFiles.includes(cleanFileName)) { // if not in the include file, skip
+                return
+            }
+        }
+
+        const fileName = extractFileName(key)
+
+        // check if the video is already in the database
+        const video = getVideo({ file_key: fileName })
+        if (!video) { // if not, insert it
+            insertVideo({ file_key: fileName, status: 'unprocessed' })
+        }
+    })
+
+    // iterate over destination videos
+    logger.info('checking videos in destination bucket, marking as done ifs exists')
+    await iterateOverDestinationVideos((key) => {
+        const fileName = extractCleanFileName(key)
+
+        // check if the video is already in the database
+        const video = getVideoByPrefix(fileName)
+        if (video && video.status !== 'completed') { // if exists, mark it as done
+            updateVideoByFileKey({ file_key: video.file_key, status: 'completed' })
+        }
+    })
+}
+
+export function setIncludeFilePath(_includeFilePath) {
+    includeFilePath = _includeFilePath
+}
