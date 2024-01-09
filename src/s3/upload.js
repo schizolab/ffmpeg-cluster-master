ea97fd3e239f737e61f083fe58a64f4faad002c0
schizolab <schizolab>
2024-01-09 16:36:22 +0800

    Check file existence in both source and destination buckets

--- src/s3/upload.js
+++ src/s3/upload.js
@@ -3,8 +3,8 @@ const logger = log4js.getLogger('s3')
 
 import { readdirSync, createReadStream, statSync } from 'fs'
 import { loadIncludeFiles, setIncludeFilePath } from '../db/init.js'
-import { sourceS3Client } from './clients.js'
-import { HeadObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { sourceS3Client, destinationS3Client } from './clients.js'
+import { HeadObjectCommand, PutObjectCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
 import { extractCleanFileName } from '../s3/utils.js';
 
 
@@ -19,25 +19,51 @@ export async function batchUpload(path) {
         }
 
         // skip if not in the include file
+        const cleanFileName = extractCleanFileName(file)
         if (includeFiles.length > 0) {
-            const cleanFileName = extractCleanFileName(file)
             if (!includeFiles.includes(cleanFileName)) {
                 continue
             }
         }
 
-        // skip if file is already in s3
-        const headObjectCommand = new HeadObjectCommand({
-            Bucket: process.env.SOURCE_S3_BUCKET,
-            Key: `${keyPrefix}${file}`
-        })
-        try {
-            await sourceS3Client.send(headObjectCommand)
-            continue // skip
-        } catch (error) {
-            // if error, file does not exist in s3
-            logger.trace(`file ${file} does not exist in s3, uploading...`)
+        // skip if the file is already in s3 destination bucket
+        {
+            const listObjectsCommand = new ListObjectsV2Command({
+                Bucket: process.env.DESTINATION_S3_BUCKET,
+                Prefix: `${process.env.DESTINATION_S3_PREFIX}${cleanFileName}.`
+            })
+            try {
+                const response = await destinationS3Client.send(listObjectsCommand)
+                const fileExists = response.Contents?.length > 0;
+
+                if (fileExists) {
+                    continue; // skip
+                } else {
+                    // if file does not exist in s3
+                    logger.trace(`file ${file} does not exist in s3 destination bucket`)
+                }
+            } catch (error) {
+                // if error, file does not exist in s3
+                logger.error(`failed to list objects in s3 destination bucket error: ${error}`)
+            }
         }
+
+        // skip if file is already in s3 source bucket
+        {
+            const headObjectCommand = new HeadObjectCommand({
+                Bucket: process.env.SOURCE_S3_BUCKET,
+                Key: `${process.env.SOURCE_S3_PREFIX}${file}`
+            })
+            try {
+                await sourceS3Client.send(headObjectCommand)
+                continue // skip
+            } catch (error) {
+                // if error, file does not exist in s3
+                logger.trace(`file ${file} does not exist in s3 source bucket`)
+            }
+        }
+
+        logger.info(`uploading file ${file}`)
 
         // upload file to S3 using stream
         const fileFullPath = `${path}${file}`
@@ -45,7 +71,7 @@ export async function batchUpload(path) {
         const fileStats = statSync(fileFullPath)
         const putObjectCommand = new PutObjectCommand({
             Bucket: process.env.SOURCE_S3_BUCKET,
-            Key: `${keyPrefix}${file}`,
+            Key: `${process.env.SOURCE_S3_PREFIX}${file}`,
             Body: fileStream,
             ContentLength: fileStats.size
         })
